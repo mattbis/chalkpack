84740eac1db141483b4681c9edffe8081f03be94
Matthew <863968+mattbis@users.noreply.github.com>
2025-01-22 16:48:56 +0000

    fix: and briefly think a bit more ( not working probably

--- src/chalkpack.mjs
+++ src/chalkpack.mjs
@@ -5,38 +5,51 @@ import {default as isString} from "is-string"
 const emptyObject = {}
 
 export const map = {
-  error: {c:"red",a:["e"]},
-  info: {c:"white",a:["i"]},
-  head: {c:"cyan",a:["h"]},
-  warn: {c:"yellow",a:["w"]},
-  yes: {c:"green",a:["y"]},
-  no: {c:"gray",a:["n"]}
+  fatal: {c:"red", a:["f"]},
+  error: {c:"red", a:["e"]},
+  info: {c:"white", a:["i"]},
+  head: {c:"cyan", a:["h"]},
+  warn: {c:"yellow", a:["w"]},
+  yes: {c:"green", a:["y"]},
+  no: {c:"gray", a:["n"]}
 }
 
 let _cached = {}
+
+// 1: make it work just node for now
+// 2: then do the other stuff... plenty of much better loggers exist.. the only real reason for this is just its tiny / apps / cli / runtime elec && the main reason is I just want something very simple.. l.br() l.i() l.head() that is it .. nothing but a simple palette... to many Ts/Js packages and apps are huge...
 
 // TODO: detect the host as browser / not a browser
 // TODO: prototype pollutions
 // TODO: c,a,r to change what happens
 // TODO: whether atomic structures or shared things are a good idea.. probably not
+// TODO: the check for existence is particularly good...
+// TODO: saveLogs
 
 //   __runTimeConsoleRef = console
 //function putLog() {}
 //function pushLogs() {}
 
 export let l = {
+  // TODO if its 'a' then this needs to change
   br: () => {console.log("")}
   // ==> br: () => {putLog()}
 }
 
+// allow it to be let... which isn't a great idea, but if the code is protected
+// and the app is local... id rather its easy to change...
 export let DEFAULT_HANDLE_LOG_OPTIONS = {
-  timings: false,
-  prefix: "--> ",
+  timing: false,
+  prefix: "--> ", // ==> --> [thing]
   preBr: false, postBr: false
 }
+
+// the console is used its not persisted
 export let DEFAULT_TARGET_OPTION = {target: 'console'}
+// the logs go to arrays... you have to save them
 export let DEFAULT_TARGET_ARRAY = {target: 'array' }
 export let DEFAULT_TARGET_ROLLING_BUFFER = {target: 'rolling'}
+export let DEFAULT_TARGET_CIRCULAR_BUFFER = {target: 'circular'}
 
 export async function _handleLogBrowser({}) {}
 export async function _handleLogServer({}) {}
@@ -49,13 +62,12 @@ export async function _handleLog({
 }) {
   const postLog = console[level] // cache local
   if (!level) level = "log"
-  if (level === "h" || level === "head") {
-    if (!options.preBr) options.preBr = $True
-    if (!options.postBr) options.postBr = $True
-  }
-
   if (!isString(level)) level.toString()
-  if (!msgs) msgs = ""
+  if (level === "h" || level === "head") {
+    if (!options.preBr) options.preBr = true
+    if (!options.postBr) options.postBr = true
+  }
+  if (!msgs) msgs = "[log]"
 
   // first get the type of the messages
   //const messagesType = Object.prototype.toString.call(msgs)
@@ -82,7 +94,7 @@ export async function _handleLog({
   return Promise.resolve()
 }
 
-// TODO: this isn't really a great idea, performance wise... 
+// TODO: this isn't really a great idea, performance wise...
 export function _proxyLogs(options) {
   return async (...args) => {
     await _handleLog({
@@ -100,17 +112,18 @@ export function chalkpack({options = {timings: false}}) {
   });
   return l;
 }
-
+export function _persistLogs() {}
 export function isRegistered() {
   return globalThis["l"].chalk_pack_now && globalThis["l"].id.includes("chalkpack")
 }
 
 export function canRegister() {
-  return !globalThis["l"]
+  return !globalThis["l"] && !isRegistered()
 }
 
 // use this in DEV mode chains so that you can avoid some insane problems... chalkpack is always going to use l.
-export function safeRegisterChalkpack() {
+// I'm marking this underscore, in that its unlikely to be used much and is more debug the tool chain
+export function _safeRegisterChalkpack() {
   if (!canRegister()) {
     console.error('cannot register chalkpack: exiting')
     return
